ea33c76154010450384d333283baa35699be5999
Jannik Pfeifer <jannik.pfeifer@accenture.com>
2019-07-03 13:47:53 +0200

    Corrected delivery shell script

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -19,8 +19,3 @@ echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 set +x
-
-echo 'The following command runs and outputs the execution of your Java'
-echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
-set -x
-java -jar target/${NAME}-${VERSION}.jar
